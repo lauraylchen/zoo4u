42a0ab8d8f1e14cdda97498d569991a412466cb1
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-22 11:42:04 -0500

    Added validation to booking model and all the association

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,4 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :animal
+
+  validates :start_date, comparison: { less_than_or_equal_to: :end_date }
+  validate :status, inclusion: { in: (0..2) }
+
+  enum status: {
+    pending: 0,
+    confirmed: 1,
+    declined: 2
+  }
 end
