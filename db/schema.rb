caf7b0cbfcf68e7d60bbeaae1c5bea6d92ae35ee
Cameron Woolf <rayadevinfo@gmail.com>
2022-02-22 14:02:28 -0500

    Cloudinary seeding template finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_182228) do
+ActiveRecord::Schema.define(version: 2022_02_22_185131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 2022_02_22_182228) do
   create_table "animals", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
-    t.string "animal_type"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "animal_type"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
