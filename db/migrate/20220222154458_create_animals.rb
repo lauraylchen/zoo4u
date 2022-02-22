7b29a8d193181f0e87544878184cd4f975548d4e
Cameron Woolf <rayadevinfo@gmail.com>
2022-02-22 10:46:01 -0500

    Animals model generated

---
+++ db/migrate/20220222154458_create_animals.rb
@@ -0,0 +1,12 @@
+class CreateAnimals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :animals do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :type
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
