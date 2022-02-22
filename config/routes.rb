063cb51042694984aeb6f8f114a95d6dbc911e51
Cameron Woolf <rayadevinfo@gmail.com>
2022-02-22 17:23:16 -0500

    as name update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :animals, only: %i[index show]
-  
-  get '/my_bookings', to: 'bookings#my_bookings', as: :show_my_bookings
+
+  get '/my_bookings', to: 'bookings#my_bookings', as: :my_bookings
 end
