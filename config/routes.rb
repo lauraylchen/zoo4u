abba17c125e0f160a167149463887bb4a3aed79c
Cameron Woolf <rayadevinfo@gmail.com>
2022-02-22 17:18:36 -0500

    Updating bookings route to my_bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
+  
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :animals, only: %i[index show]
+  
+  get '/my_bookings', to: 'bookings#my_bookings', as: :show_my_bookings
 end
