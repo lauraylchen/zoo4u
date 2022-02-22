5ebcd9bd9aa51a6d52a78942ed08e3f889ca88c1
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-02-22 14:26:52 -0500

    Ran the db migrate and yarn and bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -25,18 +25,6 @@ ActiveRecord::Schema.define(version: 2022_02_22_185131) do
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
-  create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "animal_id", null: false
-    t.date "start_date", null: false
-    t.date "end_date", null: false
-    t.integer "status", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["animal_id"], name: "index_bookings_on_animal_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +39,4 @@ ActiveRecord::Schema.define(version: 2022_02_22_185131) do
   end
 
   add_foreign_key "animals", "users"
-  add_foreign_key "bookings", "animals"
-  add_foreign_key "bookings", "users"
 end
