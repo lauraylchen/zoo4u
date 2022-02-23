f42734fe2b277559834a463337bcba04c5e8af91
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-23 11:10:38 -0500

    Changed the validation and the view index

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,6 @@ class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :animal_type, :description, presence: true
+  validates :name, :animal_type, :description, :price, presence: true
   has_one_attached :photo
 end
