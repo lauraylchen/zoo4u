b643f6c74dc43d939c113061a99fcc2ea66292fa
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-23 11:29:43 -0500

    Added form page to post an animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,4 +8,24 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user = current_user
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :animal_type, :description, :price, :photo_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :animals, only: %i[index show]
+  resources :animals, only: %i[index show new create]
 
   get '/my_bookings', to: 'bookings#my_bookings', as: :my_bookings
 end
