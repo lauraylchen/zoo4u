b99c645b0229ea4e96174e28895f971a69bf1296
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-23 12:19:22 -0500

    Completed the form for booking an animal

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,38 @@
 class BookingsController < ApplicationController
-  
+
   # def index
   #   @bookings = Booking.all
   # end
 
   def my_bookings
     @bookings = Booking.where(id: current_user.id)
+  end
+
+  def new
+    set_animal
+    @booking = Booking.new
+  end
+
+  def create
+    set_animal
+    @booking = Booking.new(booking_params)
+    @booking.animal = @animal
+    @booking.user = current_user
+    if @booking.save
+      redirect_to my_bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_animal
+    @animal = Animal.find(params[:animal_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 
 end
