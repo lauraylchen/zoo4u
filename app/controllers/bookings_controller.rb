71549909792ce0cffd28a302b08503b7ffec8571
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-25 14:53:19 -0500

    Changed the card for the bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,9 +6,7 @@ class BookingsController < ApplicationController
 
   def my_bookings
     @bookings = Booking.where(user_id: current_user.id).reverse
-    # @owner_bookings = current_user.owner_bookings.sort_by(&:status).reverse
-    @pendings = current_user.owner_bookings.select { |booking| booking.status == "pending" }.reverse
-    @owner_bookings = current_user.owner_bookings.reject { |booking| booking.status == "pending" }.reverse
+    @owner_bookings = current_user.owner_bookings.sort_by(&:status).reverse
   end
 
   def create
